ad1a91b9e55e71a0b83c246e9e3e04fbbd3bb741
AnaMM <amlcm31@gmail.com>
2025-07-13 14:51:00 -0600

    counting letters and allowing for other book arguments

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from stats import get_num_words
 from stats import get_num_characters
+from stats import get_sorted_characters
+import sys
 
 #new function to get filepath as input and return it's contents as a string
 def get_book_text(filepath):
     # do something with f (the file) here
     with open(filepath) as f:
         # f is a file object
+        # convert the book text to a string
         file_contents = f.read()
-    # convert the book text to a string
+    
     return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     num_words = get_num_words(book_text)
     num_characters = get_num_characters(book_text)
-    print(f"{num_characters} words found in the document")
+    sorted_characters = get_sorted_characters(num_characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_characters:
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -20,5 +20,24 @@ def get_num_characters(file_contents):
             character_count[char] = 1
 
     return character_count
-            
 
+# This function takes ONE dictionary (which will be an item from your list)
+# and returns the value (the number) that you want to sort by.
+def sort_on(dictionary_item):
+        return dictionary_item["num"]
+   
+            
+# takes a dictionary of characters and their counts and returns a sorted list
+def get_sorted_characters(character_count):
+    
+    character_dictionary_list = []
+
+    # add new_character_dictionary to character_dictionary_list
+    for char, num in character_count.items():
+        if char.isalpha():
+            character_dictionary = {"char" : char, "num" : num}
+            character_dictionary_list.append(character_dictionary)
+    
+    character_dictionary_list.sort(reverse=True, key= sort_on)
+    
+    return character_dictionary_list
